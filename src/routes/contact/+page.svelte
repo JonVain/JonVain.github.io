2380f2793dec46ea62dba119d361e68b92199641
JonVain <jonne.vainikainen@edu.savonia.fi>
2023-03-06 01:19:46 +0200

    fiiiiixx

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,25 +1,31 @@
-<h1>Yhteystiedot</h1>
-<p id="yt">Jonne Vainikainen</p>
-<b />
-<hr
-    class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"
-/>
-<p id="testink">Jätä palautetta alla olevaan kenttään.</p>
-
-<div class="form-floating">
-    <textarea
-        class="form-control"
-        placeholder="Leave a comment here"
-        id="floatingTextarea2"
-        style="height: 100px"
+<div id="taustakuva">
+    <h1>Yhteystiedot</h1>
+    <p id="yt">Jonne Vainikainen</p>
+    <b />
+    <hr
+        class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"
     />
-    <label for="floatingTextarea2">Comments</label>
-</div>
-<div>
-    <button type="button" class="btn btn-primary">Lähetä</button>
+    <p id="testink">Jätä palautetta alla olevaan kenttään.</p>
+
+    <div class="form-floating">
+        <textarea
+            class="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea2"
+            style="height: 100px"
+        />
+        <label for="floatingTextarea2">Comments</label>
+    </div>
+    <div>
+        <button type="button" class="btn btn-primary">Lähetä</button>
+        <h2>Nappi on visuaalinen</h2>
+    </div>
 </div>
 
 <style>
+    h2 {
+        text-align: left;
+    }
     h1,
     p {
         text-align: center;
@@ -38,5 +44,11 @@
     }
     #yt {
         word-wrap: break-word;
+    }
+    #taustakuva {
+        background-image: url(kuubg.jpg);
+        height: 200vh;
+        background-size: auto;
+        background-repeat: no-repeat;
     }
 </style>
